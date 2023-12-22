9a7cde2ded1e666c25da57c0490f89f1b5f2f78d
Ryan Mossor <ryan.mossor@gmail.com>
2023-12-21 21:56:40 -0600

    Day 8 part 2

--- day08/day08.go
+++ day08/day08.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"regexp"
@@ -61,6 +62,89 @@ func partOne(lines []string) error {
 
     fmt.Println("Part 1:", steps)
     return nil
+}
+
+func partTwo(lines []string) error {
+    var regex = regexp.MustCompile(`(?P<node>[A-Z]{3}) = \((?P<left>[A-Z]{3}), (?P<right>[A-Z]{3})\)`)
+    instructions := lines[0]
+    nodeMap := make(map[string]Node)
+
+    for _, line := range lines {
+        match := regex.FindStringSubmatch(line)
+
+        if len(match) == 0 {
+            continue
+        }
+
+        node, left, right := match[1], match[2], match[3]
+        n := Node { left, right }
+        nodeMap[node] = n
+    }
+
+    var startingNodes []string
+    for k := range nodeMap {
+        if strings.HasSuffix(k, "A") {
+            startingNodes = append(startingNodes, k)
+        }
+    }
+
+    steps := []int {}
+    for _, node := range startingNodes {
+        s := calcSteps(node, nodeMap, instructions)
+        steps = append(steps, s)
+    }
+
+    result := lcm(steps...)
+    fmt.Println("Part 2:", result)
+
+    return nil
+}
+
+func calcSteps(node string, nodeMap map[string]Node, instructions string) int {
+    steps, endsWithZ := 0, false
+    for !endsWithZ {
+        for _, ch := range instructions {
+            if string(ch) == "L" {
+                node = nodeMap[node].L
+            } else {
+                node = nodeMap[node].R
+            }
+
+            steps++
+
+            if strings.HasSuffix(node, "Z") {
+                endsWithZ = true
+                break
+            }
+        }
+    }
+
+    return steps
+}
+
+func gcd(a, b int) int {
+    for b != 0 {
+        t := b
+        b = a % b
+        a = t
+    }
+
+    return a
+}
+
+func lcm(ints ...int) int {
+    if len(ints) < 2 {
+        panic(errors.New("Not enough arguments"))
+    }
+
+    a, b := ints[0], ints[1]
+    result := a * b / gcd(a, b)
+
+    for i := 0; i < len(ints[2:]); i++ {
+        result = lcm(result, ints[2:][i])
+    }
+
+    return result
 }
 
 func readLines(path string) ([]string, error) {
